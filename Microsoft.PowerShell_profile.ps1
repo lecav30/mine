fee380b4081af31356f6340744a548441db87c7c
Sebastián Lévano <79949221+lecav30@users.noreply.github.com>
2022-07-01 16:51:24 -0500

    PowerShell 7 Settings

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+# oh-my-posh --init --shell pwsh --config C:/Users/lecav/AppData/Local/Programs/oh-my-posh/themes/blue-owl.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config C:/Users/lecav/OneDrive/blue-owl.omp.json | Invoke-Expression
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module terminal-icons
+
+# PSReadLine
+# Para ver las keybinds disponibles:
+# Get-PSReadlineKeyHandler
+Set-PSReadLineOption -PredictionSource History
+# Para ver las sugerencias en una lista:
+# Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Alias
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name touch -Value New-Item
