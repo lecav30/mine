052c10dc2ef7981ff3593f09903f02fa0958b667
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2022-09-11 17:13:03 -0500

    Add Z, Terminal-Icons, PSReadLine

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,21 @@
+# Oh my posh
+oh-my-posh init pwsh | Invoke-Expression
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\di4am0nd.omp.json" --print) -join "`n"))
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+# Z
+
+# Alias
 Set-Alias -Name v -Value nvim
 Set-Alias -Name vi -Value nvim
+Set-Alias -Name ll -Value ls
 Set-Alias -Name touch -Value New-Item
-
 Set-Alias -Name pdw -Value pwd
-
 Set-Alias -Name g -Value git
 
-oh-my-posh init pwsh | Invoke-Expression
-
-& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\di4am0nd.omp.json" --print) -join "`n"))
