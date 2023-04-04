232fa7993aeb9e75a806887f188fb7cd84b9e68e
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2023-04-03 20:16:04 -0500

    New commands

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -3,18 +3,15 @@
 # Z
 # PSReadLine
 # Terminal-Icons
-# Z
+
 # Scripts:
 # Winfetch
  
 # Oh my posh
 oh-my-posh init pwsh | Invoke-Expression
-# & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha.omp.json" --print) -join "`n"))
-# & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha_lecav.omp.json" --print) -join "`n"))
-& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:OneDrive\takuya_modify.omp.json" --print) -join "`n"))
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:OneDrive\catppuccin_mocha_lecav.omp.json" --print) -join "`n"))
 
 # PSReadLine
-# https://github.com/MicrosoftDocs/PowerShell-Docs/blob/main/reference/5.1/PSReadLine/Get-PSReadLineOption.md
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Emacs
@@ -23,29 +20,21 @@ Set-PSReadLineOption -BellStyle None
 # Terminal-Icons
 Import-Module -Name Terminal-Icons
 
-# Z
-
 # Winfetch
 Set-Alias winfetch pwshfetch-test-1 
 
-# Alias
-Set-Alias -Name v -Value nvim
 Set-Alias -Name vi -Value nvim
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name lg -Value lazygit
+
 Set-Alias -Name ll -Value ls
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name pdw -Value pwd
-Set-Alias -Name g -Value git
 Set-Alias -Name cls -Value clear
 
-# Start
-# winfetch
+Set-Alias -Name whr -Value where.exe
 
 # Utilities
-function which ($command) {
-        Get-Command -Name $command -ErrorAction SilentlyContinue |
-        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-    }
-
 function vimfiles ($command) {
         Set-Location "C:\Users\lecav\AppData\Local\nvim"
     }
@@ -54,7 +43,19 @@ function home ($command) {
         Set-Location "$env"
     }
 
+function programs ($command) {
+        Set-Location "E:\Programs"
+    }
+
 function jsonsvr ($command) {
         json-server --watch server/db.json --routes server/routes.json
+    }
+
+function mysql-c ($command) {
+        mysql -u root -pdatabase2002
+    }
+
+function parrot ($command) {
+        curl parrot.live
     }
 
