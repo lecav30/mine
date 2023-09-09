dd7862096b6769eb05b0eef36b926b02fc273494
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2023-09-09 17:50:10 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,6 @@ export EDITOR='nvim'
 source "$HOME/.config/fish/aliases.fish"
 source "$HOME/.config/fish/git_prompt.fish"
 
+set PATH $PATH ~/.cargo/env;
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
