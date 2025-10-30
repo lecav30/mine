2fcad20e974e1db95f91c0db6774ec929ad10d97
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2025-10-29 22:16:46 -0500

    Update .wezterm.lua

--- Windows/.wezterm.lua
+++ Windows/.wezterm.lua
@@ -6,39 +6,29 @@ local config = wezterm.config_builder()
 
 config = {
 	automatically_reload_config = true,
-	-- josean-dev coolnight colorscheme
-	-- colors = {
-	-- 	foreground = "#CBE0F0",
-	-- 	background = "#011423",
-	-- 	cursor_bg = "#47FF9C",
-	-- 	cursor_border = "#47FF9C",
-	-- 	cursor_fg = "#011423",
-	-- 	selection_bg = "#033259",
-	-- 	selection_fg = "#CBE0F0",
-	-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-	-- }
-	color_scheme = "Catppuccin Mocha",
+	adjust_window_size_when_changing_font_size = false,
+    color_scheme = 'Catppuccin Mocha',
 	-- Font
-	font = wezterm.font("JetBrainsMono Nerd Font Propo"),
-	font_size = 14,
+	-- font = wezterm.font("JetBrainsMono Nerd Font Propo"),
+	font = wezterm.font("iMWritingMono Nerd Font Propo"),
+	font_size = 12,
 	-- Shell
 	default_prog = { "pwsh.exe", "-NoLogo" },
 	-- Tab
 	-- enable_tab_bar = false,
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
+	-- use_fancy_tab_bar = false,
+	-- tab_bar_at_bottom = true,
 	--Cursor
 	default_cursor_style = "SteadyBar",
 	-- Window
 	window_decorations = "RESIZE",
-	window_background_opacity = 0.85,
+	window_background_opacity = 1,
 	macos_window_background_blur = 10,
 	window_padding = {
 		left = 10,
 		right = 10,
-		top = 0,
-		bottom = 0,
+		top = 10,
+		bottom = 10,
 	},
 	-- Hyperlink, from: https://akos.ma/blog/adopting-wezterm/
 	hyperlink_rules = {
